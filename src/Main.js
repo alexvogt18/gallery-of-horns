dd60f99a54f931ce49d40f51bf15ac4026273784
Alex Vogt <alexvogt18@gmail.com>
2021-06-10 22:30:16 -0700

    Lab 4

--- src/Main.js
+++ src/Main.js
@@ -2,14 +2,13 @@ import React from 'react';
 import './App.css';
 import Beast from './HornedBeast.js';
 import CardColumns from 'react-bootstrap/CardColumns';
-import Data from './resources/data.json';
 
 
 class Main extends React.Component {
   render(){
     return (
       <CardColumns className="Main">
-        {Data.map( (item) => {return <Beast src = {item.src} title = {item.title} description = {item.description} alt = {item.alt} />})}
+        {this.props.data.map( (item) => {return <Beast src = {item.src} title = {item.title} description = {item.description} alt = {item.alt} />})}
       </CardColumns>
     );
   };
